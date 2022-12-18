ae1c41a4c28523143d88f44a6c8b05e1488cfa34
Albert Sereda <Acehil94@gmail.com>
2022-12-18 19:54:15 +0500

    Init project: shawarma-cloud

---
+++ src/test/java/com/spring/shawarma/shawarmacloud/ShawarmaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.shawarma.shawarmacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawarmaCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
